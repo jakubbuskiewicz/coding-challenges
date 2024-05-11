8ab501e41efa93ab643e2bfe0b944896f78af7df
Jakub Buskiewicz <jakub.buskiewicz@vortexa.com>
2024-05-11 22:37:40 +0100

    feat: count words in file

--- wc/src/main.rs
+++ wc/src/main.rs
@@ -10,6 +10,12 @@ fn main() {
         let number_of_lines: usize =
             count_number_of_lines_in_file(&path).expect("could not count lines in file");
         return println!("{:?} {:?}", number_of_lines, path);
+    } else if pattern == "w" {
+        let number_of_words: usize =
+            count_number_of_words_in_file(&path).expect("could not count words in file");
+        return println!("{:?} {:?}", number_of_words, path);
+    } else {
+        panic!("unknown pattern {:?}", pattern);
     }
 }
 
@@ -39,4 +45,20 @@ fn count_number_of_lines_in_file(path: &str) -> Result<usize, std::io::Error> {
     }
 
     Ok(number_of_lines)
+}
+
+fn count_number_of_words_in_file(path: &str) -> Result<usize, std::io::Error> {
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
+
+    let file: File = File::open(path)?;
+    let reader: BufReader<File> = BufReader::new(file);
+
+    let mut number_of_words: usize = 0;
+    for line in reader.lines() {
+        let line = line?;
+        number_of_words += line.split_whitespace().count();
+    }
+
+    Ok(number_of_words)
 }
